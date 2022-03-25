b20680fa9dad973b29b45a04c0627acff2fe52f0
Olivier <hangar18rip@users.noreply.github.com>
2022-03-25 13:54:46 +0100

    update repo

--- js/src/lib/es2022.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference lib="es2021" />
-/// <reference lib="es2022.array" />
-/// <reference lib="es2022.error" />
-/// <reference lib="es2022.object" />
-/// <reference lib="es2022.string" />
--- js/src/lib/esnext.intl.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Intl {
-  interface NumberFormat {
-    formatRange(start: number | bigint, end: number | bigint): string;
-    formatRangeToParts(start: number | bigint, end: number | bigint): NumberFormatPart[];
-  }
-}
